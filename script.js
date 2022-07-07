723b6fae1d5bf558ba901f368b7ed0781b4d069b
Divyansh Singh <kalki@Divyanshs-MacBook-Air.local>
2022-07-08 05:17:32 +0530

    Solve linter errors

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-
 let books = [];
 if (localStorage.books) books = JSON.parse(localStorage.getItem('books'));
 
-function deletebook(event) {
-    const indexn = event.currentTarget.dataset.index;
-    books.splice(parseInt(deletebook), 1);
-    additems();
-    localStorage.setItem('books', JSON.stringify(books));
-}
+const creatli = document.getElementById('list-cr');
 
-const creatli = document.getElementById("list-cr");
-
-function additems () {
-    let cards = '';
-    for(let i = 0; i<books.length; i++) {
-        const items = `
+function additems() {
+  let cards = '';
+  for (let i = 0; i < books.length; i += 1) {
+    const items = `
         <ul>
         <li>${books[i].title}</li>
         <li>${books[i].author}</li>
@@ -22,34 +14,34 @@ function additems () {
         </ul>
         `;
 
-        cards += items;
-    }
+    cards += items;
+  }
 
-    creatli.innerHTML=cards;
+  creatli.innerHTML = cards;
 
-    document.querySelectorAll('.remove').forEach((element) => 
-    element.addEventListener('click', deletebook));
+  document.querySelectorAll('.remove').forEach((element) => element.addEventListener('click', (event) => {
+    const indexn = event.currentTarget.dataset.index;
+    books.splice(parseInt(indexn, 5), 1);
+    additems();
+    localStorage.setItem('books', JSON.stringify(books));  
+  }
+  ));
 }
 
 additems();
 
-const titin = document.getElementById("title");
-const autna = document.getElementById("author");
-
+const titin = document.getElementById('title');
+const autna = document.getElementById('author');
 
 function addBook() {
-
-    const book = {};
-    book.title = titin.value;
-    book.author = autna.value;
-    books.push(book);
-    additems();
-    localStorage.setItem('books', JSON.stringify(books));
+  const book = {};
+  book.title = titin.value;
+  book.author = autna.value;
+  books.push(book);
+  additems();
+  localStorage.setItem('books', JSON.stringify(books));
 }
 
-const btnadd = document.getElementById("add");
+const btnadd = document.getElementById('add');
 
 btnadd.addEventListener('click', addBook);
-
-
-
